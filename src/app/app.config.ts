24f713d3f2b2c5ea3f4295436a518ab8408e063d
Daniel Enriquez <daniel.enriquezmontalvo@gmail.com>
2024-10-02 17:03:27 -0700

    interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,7 @@ import {ApplicationConfig, importProvidersFrom} from "@angular/core";
 import {provideRouter} from "@angular/router";
 import {routes} from "./app-routing.module";
 import {provideHttpClient, withInterceptors} from "@angular/common/http";
-import {baseUrlInterceptor} from "./share/interceptor/base-url.interceptor";
 import {provideAnimations} from "@angular/platform-browser/animations";
-import {tokenInterceptor} from "./share/interceptor/token.interceptor";
-import {errorInterceptor} from "./share/interceptor/error.interceptor";
 import {DialogService} from "primeng/dynamicdialog";
 import {NgxPermissionsModule} from "ngx-permissions";
 
@@ -14,7 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimations(),
     provideHttpClient(
-      withInterceptors([baseUrlInterceptor, tokenInterceptor, errorInterceptor]),
+      withInterceptors([ ]),
     ),
     importProvidersFrom(NgxPermissionsModule.forRoot()),
     DialogService
