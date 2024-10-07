0f6f93306d496e7d143a107525e1dcd3379fdfeb
Daniel Enriquez <daniel.enriquezmontalvo@gmail.com>
2024-10-07 10:03:51 -0700

    navbar

--- src/app/share/components/navbar/navbar.component.ts
+++ src/app/share/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import {AfterViewInit, Component} from '@angular/core';
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-navbar',
@@ -12,10 +13,51 @@ import {RouterLink} from "@angular/router";
 })
 export class NavbarComponent implements AfterViewInit {
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngAfterViewInit() {
+    this.initializeNavbar();
   }
 
 
+  private initializeNavbar() {
+    // Navbar shrink function
+    const navbarShrink = () => {
+      const navbarCollapsible = document.body.querySelector('#mainNav');
+      if (!navbarCollapsible) {
+        return;
+      }
+      if (window.scrollY === 0) {
+        navbarCollapsible.classList.remove('navbar-shrink');
+      } else {
+        navbarCollapsible.classList.add('navbar-shrink');
+      }
+    };
+
+    // Shrink the navbar
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+      new bootstrap.ScrollSpy(document.body, {
+        target: '#mainNav',
+        rootMargin: '0px 0px',
+      });
+    }
+
+    const navbarToggler = document.body.querySelector('.navbar-toggler') as HTMLElement;
+    const responsiveNavItems = Array.from(document.querySelectorAll('#navbarResponsive .nav-link')) as HTMLElement[];
+
+    responsiveNavItems.forEach(responsiveNavItem => {
+      responsiveNavItem.addEventListener('click', () => {
+        if (window.getComputedStyle(navbarToggler).display !== 'none') {
+          navbarToggler.click();
+        }
+      });
+    });
+  }
 }
