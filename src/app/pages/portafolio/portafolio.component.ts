d624cc47ac86f9e749d8096fdd0b760265109341
Daniel Enriquez <daniel.enriquezmontalvo@gmail.com>
2024-10-21 17:08:36 -0700

    hace sueño

--- src/app/pages/portafolio/portafolio.component.ts
+++ src/app/pages/portafolio/portafolio.component.ts
@@ -12,5 +12,34 @@ import {NgClass, NgIf} from "@angular/common";
   styleUrl: './portafolio.component.css'
 })
 export class PortafolioComponent {
+  activeTab: string = 'preElectoral';
+  tabTitle: string = 'PRE ELECTORAL';
+  tabContent: string = `
+    Consolidamos la información estatal en cada una de las etapas del proceso electoral,
+    ofreciendo la oportunidad de evaluar con prontitud y exactitud el desarrollo del proceso.
+  `;
 
+  changeContent(tab: string) {
+    this.activeTab = tab;
+
+    if (tab === 'preElectoral') {
+      this.tabTitle = 'PRE ELECTORAL';
+      this.tabContent = `
+        Consolidamos la información estatal en cada una de las etapas del proceso electoral,
+        ofreciendo la oportunidad de evaluar con prontitud y exactitud el desarrollo del proceso.
+      `;
+    } else if (tab === 'postElectoral') {
+      this.tabTitle = 'POST ELECTORAL';
+      this.tabContent = `
+        Evaluamos los resultados del proceso electoral, asegurando la transparencia y precisión
+        de la información presentada.
+      `;
+    } else if (tab === 'evaluacion') {
+      this.tabTitle = 'EVALUACIÓN';
+      this.tabContent = `
+        Realizamos una evaluación integral de todo el proceso, identificando áreas de mejora
+        para futuros eventos electorales.
+      `;
+    }
+  }
 }
